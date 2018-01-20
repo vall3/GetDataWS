2aec2225754c6bcab16e67e30fdcfcb37f0eb4f9
Daniel <dvillaj@gmail.com>
2018-01-20 13:39:04 +0100

    Se corrige el shell script

--- download.sh
+++ download.sh
@@ -1,2 +1,4 @@
+#!\bin\bash
+
 python get_data.py https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Municipios/
 hadoop fs -put Municipios.json /ingest/Municipios
