4c998d983b411ed7f3885353750125168cdf8e7d
Daniel Villanueva <dvillaj@gmail.com>
2018-01-20 02:12:26 -0800

    Datos

---
+++ download.sh
@@ -0,0 +1,2 @@
+python get_data.py https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Municipios/
+hadoop fs -put Municipios.json /ingest/Municipios
--- get_data.py
+++ get_data.py
@@ -6,21 +6,21 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('url', help="Dirección del servicio Web")
+parser.add_argument('url', help="Direccion del servicio Web")
 parser.add_argument("-d", "--dir", help="Directorio donde se van a grabar los datos", default = ".")
-parser.add_argument("-s", "--sep", help="Separador de los campos (Sólo CSV)", default = "|")
+parser.add_argument("-s", "--sep", help="Separador de los campos (Solo CSV)", default = "|")
 parser.add_argument("-i", "--item", help="Elemento de la respuesta donde se encuentran los datos")
 parser.add_argument("-n", "--name", help="Nombre del fichero")
 parser.add_argument("-f", "--format", help="Formato de los datos (json o csv)", default = "json")
 args = parser.parse_args()
 
 if args.url is None:
-    parser.error("Es necesario especificar la dirección del servicio web!")
+    parser.error("Es necesario especificar la direccion del servicio web!")
     sys.exit(1)
 
 valid_formats = ["csv", "json"]
 if args.format not in valid_formats:
-    parser.error("Error en el formato de los datos. Valores válidos: %s" % ','.join(valid_formats))
+    parser.error("Error en el formato de los datos. Valores validos: %s" % ','.join(valid_formats))
     sys.exit(1)
 
 if args.name is None:
